5e57ac69868f5576703f41d276a861470c883f31
KTeno <apkohl10@gmail.com>
2025-10-30 16:50:10 -0600

    Empty main window

--- SamplePlugin/Windows/MainWindow.cs
+++ SamplePlugin/Windows/MainWindow.cs
@@ -11,9 +11,6 @@ public class MainWindow : Window, IDisposable
 {
     private readonly Plugin plugin;
 
-    // We give this window a hidden ID using ##.
-    // The user will see "My Amazing Window" as window title,
-    // but for ImGui the ID is "My Amazing Window##With a hidden ID"
     public MainWindow(Plugin plugin, string goatImagePath)
         : base("Image Viewer##ImageViewerWindow", ImGuiWindowFlags.NoScrollbar | ImGuiWindowFlags.NoScrollWithMouse)
     {
@@ -30,12 +27,6 @@ public class MainWindow : Window, IDisposable
 
     public override void Draw()
     {
-        // Hello World message added here!
-        ImGui.TextUnformatted("Hello World!");
-        ImGui.Spacing();
-        
-        ImGui.TextUnformatted($"The random config bool is {plugin.Configuration.SomePropertyToBeSavedAndWithADefault}");
-
         if (ImGui.Button("Show Settings"))
         {
             plugin.ToggleConfigUi();
@@ -43,15 +34,11 @@ public class MainWindow : Window, IDisposable
 
         ImGui.Spacing();
 
-        // Normally a BeginChild() would have to be followed by an unconditional EndChild(),
-        // ImRaii takes care of this after the scope ends.
-        // This works for all ImGui functions that require specific handling, examples are BeginTable() or Indent().
         using (var child = ImRaii.Child("SomeChildWithAScrollbar", Vector2.Zero, true))
         {
-            // Check if this child is drawing
             if (child.Success)
             {
-                ImGui.TextUnformatted("This is where your content will go!");
+                
             }
         }
     }
